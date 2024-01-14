41745a59782c66c2fb5656758f6da3778806006d
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-01-14 15:44:39 -0300

    Finalizando Curso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 import { NgIfNgforComponent } from './ngIf-ngfor/ngIf-ngfor.component';
+import { CrudComponent } from './componentes/crud/crud.component';
 
 
 export const routes: Routes = [
     { path: 'primeiro', component: DataBindingComponent },
     { path: 'segundo', component: SegundoComponenteComponent },
-    // { path: 'teste', component: NgIfNgforComponent }
+    { path: 'teste', component: NgIfNgforComponent },
+    { path: 'crud', component: CrudComponent }
 
 ];
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Images } from '../models/placeholder.model';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+  @Injectable({
+    providedIn: 'root'
+  })
 export class CrudService {
 
-constructor() { }
-
-}
+  constructor(private http: HttpClient) { }
+  public getFotos(): Observable<any>{
+    return this.http.get(`https://jsonplaceholder.typicode.com/photos`);
+    }
+  }
